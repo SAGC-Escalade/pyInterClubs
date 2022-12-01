895e703682b4e7d1eb8cfbd64cd1578e770a64c0
deveil <jimmy.deveil+github@gmail.com>
2022-12-01 02:52:25 +0100

    MAJ liens vers les répertoires

--- pyInterClubs/settings.py
+++ pyInterClubs/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = Path(__file__).resolve().parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'pyInterClubs.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / "templates"],
+        'DIRS': [BASE_DIR / 'pyInterClubs/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -113,5 +113,5 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-STATIC_URL = '/static/'
-STATIC_ROOT = [BASE_DIR / 'static']
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'pyInterClubs/static']
