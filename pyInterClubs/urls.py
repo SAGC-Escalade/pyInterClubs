86f0b41e30017372ba0d1848ef8d9caa0d807010
deveil <jimmy.deveil+github@gmail.com>
2022-12-01 02:53:52 +0100

    Ajout de la première page (index) pour la composition globale de celle-ci

--- pyInterClubs/urls.py
+++ pyInterClubs/urls.py
@@ -18,9 +18,12 @@ Including another URLconf
 
 # Uncomment next two lines to enable admin:
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     # Uncomment the next line to enable the admin:
-    path('admin/', admin.site.urls)
+    path('admin/', admin.site.urls),
+    path('account/', include('django.contrib.auth.urls')),
+    path('', TemplateView.as_view(template_name="index.html")),
 ]
