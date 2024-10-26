972602b89793aa74dffd06c7c103564231815b39
deveil <jimmy.deveil+github@gmail.com>
2024-10-26 21:01:20 +0200

    Bug: Correction des regexp pour Python 3.12

--- api/urls.py
+++ api/urls.py
@@ -11,13 +11,13 @@ router.register('voies', VoieViewSet, 'voie')
 router.register('grimpeurs', GrimpeurViewSet, 'grimpeur')
 
 router.register('equipes', EquipeViewSet, 'equipe')
-router.register('club/(?P<club>\d+)/equipes', EquipeViewSet, 'club-equipe')
+router.register(r'club/(?P<club>\d+)/equipes', EquipeViewSet, 'club-equipe')
 
 router.register('scores', ScoreViewSet, 'score')
-router.register('club/(?P<club>\d+)/scores', ScoreViewSet, 'club-score')
+router.register(r'club/(?P<club>\d+)/scores', ScoreViewSet, 'club-score')
 
 router.register('perfs', PerformanceViewSet, 'perf')
-router.register('voie/(?P<voie>\d+)/perfs', PerformanceViewSet, 'voie-perf')
+router.register(r'voie/(?P<voie>\d+)/perfs', PerformanceViewSet, 'voie-perf')
 
 urlpatterns = [
     path('', include(router.urls)),
