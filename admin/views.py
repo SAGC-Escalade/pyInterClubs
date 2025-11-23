892449a9ad4e9136d7cecc6e38d42b02eadd3c1a
deveil <jimmy.deveil+github@gmail.com>
2025-11-23 16:44:04 +0100

    Bug: les compteurs sont à 0 dans les statistiques de la rencontre => Ok

--- admin/views.py
+++ admin/views.py
@@ -252,9 +252,9 @@ class StatsReportView(RencontreReportViewMixin, SuperUserRequiredMixin, DetailVi
         femmes = [s for s in inscrits if s.grimpeur.sexe==Genre.femme]
         femmes = self.ranking(sorted(femmes, key=lambda s: s.points, reverse=True))
 
-        inscrits = [s.grimpeur for s in inscrits]
-        inscrits = sorted(inscrits, key=lambda s: (s.club.nom, s.nom, s.prenom))
-        inscrits = [(c,list(g)) for c,g in groupby(inscrits, key=attrgetter('club.nom'))]
+        context['classements'] = [('hommes', hommes), ('femmes', femmes)]
+        context['equipes'] = equipes
+        context['inscrits'] = inscrits
 
         # On compte les passages dans chaques voies par genre de grimpeur et état de réussite
         counts = {}
